d129714215173ee9a3f9728ece939546bd126eea
janghyungyu <yama5993@nate.com>
2025-12-12 21:23:55 +0900

    ✨ feat(analytics): Google Analytics 설정 업데이트 및 naver-site-verification 메타 태그 추가

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,8 +1,5 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-197LKDJGPJ', {
-	linker: {
-		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev']
-	}
-});
+
+gtag('config', 'G-NR6653KS5K');
