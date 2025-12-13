f06b402c4495202cb8ff95c22ad93546a4ecc5e5
janghyungyu <yama5993@nate.com>
2025-12-13 12:57:31 +0900

    ✨ feat(analytics): GA 설정에 도메인 링크 추가

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -2,4 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-NR6653KS5K');
+gtag('config', 'G-NR6653KS5K', {
+	linker: {
+		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev', 'golf.archerlab.dev']
+	}
+});
