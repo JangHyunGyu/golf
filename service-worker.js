17a87f09ca463ee857ee5140a0b95a0afaa274fc
janghyungyu <yama5993@nate.com>
2025-12-14 02:20:20 +0900

    ✨ feat: PWA 지원을 위한 서비스 워커 및 매니페스트 추가

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", () => {
+  // intentionally left blank: all requests go straight to the network
+});
