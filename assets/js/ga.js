4a9e49b60bfb4f74fbec4530d32fd8485173c7fb
yama5993 <yama5993@nate.com>
2025-12-18 18:16:55 +0900

    ✨ feat: GA 설정에서 도메인 목록 업데이트

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -4,6 +4,6 @@ gtag('js', new Date());
 
 gtag('config', 'G-NR6653KS5K', {
 	linker: {
-		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev', 'golf.archerlab.dev']
+		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'chatbot.archerlab.dev', 'golf.archerlab.dev']
 	}
 });
