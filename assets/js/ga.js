59dcbc75379fd81dcc6f025de8adcefdb5338599
yama5993 <yama5993@nate.com>
2025-12-23 17:48:34 +0900

    ✨ feat: Google Analytics ID 및 WCS WA 코드 업데이트

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,8 +1,14 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-NR6653KS5K';
+    document.head.appendChild(script);
+})();
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-KDKK3ZZK50', {
+gtag('config', 'G-NR6653KS5K', {
 	linker: {
 		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'chatbot.archerlab.dev', 'golf.archerlab.dev', 'cupid.archerlab.dev']
 	}
